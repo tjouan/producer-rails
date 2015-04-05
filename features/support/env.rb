c7c1d823ba334868846e0193943d97059a4fbf8c
Thibault Jouan <tj@a13.fr>
2015-04-05 02:04:16 +0000

    Implement @ruby_bundler cucumber hook
      Simulate a fake gem environment in fake home directory (so it works
    through ssh), and install bundler.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,15 @@
 require 'aruba/cucumber'
 require 'producer/core/testing/cucumber'
+
+Before('@ruby_bundler') do
+  env = { 'GEM_HOME' => "#{ENV['HOME']}/.gem" }
+  write_file '.gemrc', "gem: --no-ri --no-rdoc\n"
+  write_file '.ssh/rc', <<-eoh
+export GEM_HOME=#{env['GEM_HOME']}
+export PATH=#{env['GEM_HOME']}/bin:$PATH
+    eoh
+  with_env(env) do
+    unset_bundler_env_vars
+    fail unless system 'gem install bundler > /dev/null'
+  end
+end
