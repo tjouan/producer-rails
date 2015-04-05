0d67b8e97db6ab82e6fb660b76abc39e95dc208b
Thibault Jouan <tj@a13.fr>
2015-04-05 03:21:28 +0000

    Fix rails repository creation in cucumber steps

--- features/steps/rails_steps.rb
+++ features/steps/rails_steps.rb
@@ -3,6 +3,8 @@ Given /^a rails app repository in remote directory "([^"]+)"$/ do |dir|
     [
       "rails new --database=postgresql --skip-bundle #{dir} > /dev/null",
       "git -C #{dir} init > /dev/null",
+      "git -C #{dir} config user.email bob@example",
+      "git -C #{dir} config user.name Bob",
       "git -C #{dir} add . > /dev/null",
       "git -C #{dir} commit -m 'Add generated rails app' > /dev/null"
     ].each { |cmd| fail unless system cmd }
