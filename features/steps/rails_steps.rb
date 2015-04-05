a6f81fd833b8ef9b79388a269e38950e9f8b0f7b
Thibault Jouan <tj@a13.fr>
2015-04-05 02:31:42 +0000

    Create rails app with postgresql in cucumber steps

--- features/steps/rails_steps.rb
+++ features/steps/rails_steps.rb
@@ -1,7 +1,7 @@
 Given /^a rails app repository in remote directory "([^"]+)"$/ do |dir|
   in_current_dir do
     [
-      "rails new --skip-bundle #{dir} > /dev/null",
+      "rails new --database=postgresql --skip-bundle #{dir} > /dev/null",
       "git -C #{dir} init > /dev/null",
       "git -C #{dir} add . > /dev/null",
       "git -C #{dir} commit -m 'Add generated rails app' > /dev/null"
