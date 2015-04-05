41e73c42f270a117fb130f35ced2160cc3bb2796
Thibault Jouan <tj@a13.fr>
2015-04-05 02:09:45 +0000

    Optimize cucumber fake gem environment setup
      Installing bundler and not using our local gem cache it way too slow.
    Reuse current environment (GEM_HOME) at the cost of decreased test
    reliability.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,15 +1,25 @@
 require 'aruba/cucumber'
 require 'producer/core/testing/cucumber'
 
-Before('@ruby_bundler') do
+GEMRC = "gem: --no-ri --no-rdoc\n".freeze
+
+Before('@sshd_gem_env_bundler') do
   env = { 'GEM_HOME' => "#{ENV['HOME']}/.gem" }
-  write_file '.gemrc', "gem: --no-ri --no-rdoc\n"
+  write_file '.gemrc', GEMRC
   write_file '.ssh/rc', <<-eoh
 export GEM_HOME=#{env['GEM_HOME']}
 export PATH=#{env['GEM_HOME']}/bin:$PATH
-    eoh
+  eoh
   with_env(env) do
     unset_bundler_env_vars
     fail unless system 'gem install bundler > /dev/null'
   end
+end
+
+Before('@sshd_gem_env') do
+  write_file '.gemrc', GEMRC
+  write_file '.ssh/rc', <<-eoh
+export GEM_HOME=#{ENV['GEM_HOME']}
+export PATH=#{ENV['GEM_HOME']}/bin:$PATH
+  eoh
 end
