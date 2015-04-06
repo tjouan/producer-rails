tjouan/producer-rails

commit a1beabb4d7119ac5fa68fe00e5d3f020e25657f7

Use recipe arguments instead of env DEPLOY_*

features/deploy_init.feature
@@ -3,7 +3,7 @@ Feature: `deploy_init' macro
 
   Background:
     Given a rails app repository
-    And I write a deployment recipe calling "deploy_init"
+    And I write a standard deployment recipe calling "deploy_init"
 
   Scenario: clones the app in configured remote directory
     When I execute the deployment recipe

features/arguments.feature
@@ -1,0 +1,39 @@
+@sshd @sshd_gem_env
+Feature: producer recipe arguments usage
+
+  Background:
+    Given a rails app repository
+
+  Scenario: `init' recipe argument is given
+    Given I write a standard deployment recipe
+    When I execute the deployment recipe with "init" recipe argument
+    Then the deployed app must be initialized
+
+  Scenario: `update' recipe argument is given
+    Given I make the initial deployment
+    And I make a change in the rails app repository
+    And I write a standard deployment recipe
+    When I execute the deployment recipe with "update" recipe argument
+    Then the deployed app repository must be up to date
+
+  @unicorn_kill
+  Scenario: `start' recipe argument is given
+    Given I make the initial deployment
+    And I write a standard deployment recipe
+    When I execute the deployment recipe with "start" recipe argument
+    Then the deployed app unicorn server must be running
+
+  Scenario: `stop' recipe argument is given
+    Given I make the initial deployment
+    And I start the deployed app
+    And I write a standard deployment recipe
+    When I execute the deployment recipe with "stop" recipe argument
+    Then the deployed app unicorn server must not be running
+
+  Scenario: `restart' recipe argument is given
+    Given I make the initial deployment
+    And I start the deployed app
+    And the deployed app unicorn server is running with a certain pid
+    And I write a standard deployment recipe
+    When I execute the deployment recipe with "restart" recipe argument
+    Then the deployed app unicorn server must have a different pid

features/steps/deploy_steps.rb
@@ -13,12 +13,16 @@ def deploy_recipe_write(repository, macro)
   eoh
 end
 
-def deploy_recipe_run
-  run_recipe remote: true, check: true
+def deploy_recipe_run(rargv: [])
+  run_recipe remote: true, check: true, rargv: rargv
 end
 
 Given /^I write a deployment recipe calling "([^"]+)"$/ do |macro|
   deploy_recipe_write @repository, macro
+end
+
+Given /^I write a standard deployment recipe$/ do
+  deploy_recipe_write @repository, 'deploy'
 end
 
 Given /^I make the initial deployment$/ do
@@ -39,6 +43,14 @@ end
 
 When /^I execute the deployment recipe$/ do
   deploy_recipe_run
+end
+
+When /^I execute the deployment recipe with "([^"]+)" recipe argument$/ do |arg|
+  deploy_recipe_run rargv: [arg]
+end
+
+Then /^the deployed app must be initialized$/ do
+  step 'the deployed app must have unicorn configuration'
 end
 
 Then /^the deployed app repository must be cloned$/ do

lib/producer/rails.rb
@@ -21,15 +21,13 @@ module Producer
     BUNDLER_UNSET_GROUPS  = %w[development test].freeze
 
     define_macro :deploy do |path = get(:app_path)|
-      if ENV.key? 'DEPLOY_INIT'
-        deploy_init path
-      else
-        deploy_update path
+      case recipe_argv[0]
+      when 'init'     then deploy_init
+      when 'update'   then deploy_update
+      when 'start'    then deploy_start
+      when 'stop'     then deploy_stop
+      when 'restart'  then deploy_restart
       end
-
-      assets_update path if set? :assets_update
-
-      deploy_restart
     end
 
     define_macro :deploy_init do |path = get(:app_path)|
@@ -44,6 +42,7 @@ module Producer
       db_seed         path if set? :db_seed
       secrets_init    path
       www_config      path
+      assets_update   path if set? :assets_update
     end
 
     define_macro :deploy_update do |path = get(:app_path)|
@@ -52,15 +51,12 @@ module Producer
       db_migrate      path
       db_seed         path if set? :db_seed
       www_config      path
+      assets_update   path if set? :assets_update
     end
 
     define_macro :deploy_restart do |path = get(:app_path)|
-      if ENV.key?('DEPLOY_INIT') || ENV.key?('DEPLOY_START')
-        deploy_start path
-      else
-        deploy_stop path
-        deploy_start path
-      end
+      deploy_stop path
+      deploy_start path
     end
 
     define_macro :deploy_stop do |path = get(:app_path)|

commit f4dfa18aeaeca658f4f6ed1664d6faea5e6a0141

Fix cucumber error introduced in a1beabb4

features/deploy_init.feature
@@ -3,7 +3,7 @@ Feature: `deploy_init' macro
 
   Background:
     Given a rails app repository
-    And I write a standard deployment recipe calling "deploy_init"
+    And I write a deployment recipe calling "deploy_init"
 
   Scenario: clones the app in configured remote directory
     When I execute the deployment recipe