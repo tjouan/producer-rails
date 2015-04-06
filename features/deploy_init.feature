f4a687635b013cf5364a04ddd467c7dc7b6c6789
Thibault Jouan <tj@a13.fr>
2015-04-06 03:51:35 +0000

    Refactor cucumber steps testing deployed app

--- features/deploy_init.feature
+++ features/deploy_init.feature
@@ -7,44 +7,25 @@ Feature: `deploy_init' macro
 
   Scenario: clones the app in configured remote directory
     When I successfully execute the recipe on remote target
-    Then the remote file "deploys/my_app/config.ru" must exist
+    Then the deployed app repository must be cloned
 
   Scenario: configures the database connection
     When I successfully execute the recipe on remote target
-    Then the remote file "deploys/my_app/config/database.yml" must contain exactly:
-      """
-      default: &default
-        adapter: postgresql
-        encoding: unicode
-        pool: 5
-
-      production:
-        <<: *default
-        database: some_host_test
-
-      """
+    Then the deployed app must have its database connection configured
 
   Scenario: installs dependencies with bundler
     When I successfully execute the recipe on remote target
-    Then the remote file "deploys/my_app/Gemfile.lock" must exist
+    Then the deployed app must have its dependencies installed
 
   Scenario: executes database migrations
     Given database does not exist
     When I successfully execute the recipe on remote target
-    Then database migrations for "deploys/my_app" must be up
+    Then the deployed app must have its database migrations up
 
   Scenario: generates a secret key for production
     When I successfully execute the recipe on remote target
-    Then secret key for "deploys/my_app" must be set
+    Then the deployed app must have secret key setup
 
   Scenario: configures unicorn server
     When I successfully execute the recipe on remote target
-    Then the remote file "deploys/my_app/config/unicorn.rb" must contain exactly:
-      """
-      worker_processes  2
-      timeout           60
-      preload_app       false
-      pid               'tmp/run/www.pid'
-      listen            "#{ENV['HOME']}/deploys/my_app/tmp/run/www.sock"
-
-      """
+    Then the deployed app must have unicorn configuration
--- features/steps/deploy_steps.rb
+++ features/steps/deploy_steps.rb
@@ -1,11 +1,13 @@
 RECIPE_PATH = 'recipe.rb'.freeze
+DEPLOY_PATH = 'deploys/my_app'.freeze
 
 def deploy_recipe_write(repository, macro)
+  @deploy_path = DEPLOY_PATH
   write_file RECIPE_PATH, <<-eoh
     require 'producer/rails'
 
     set :repository,  '#{repository}'
-    set :app_path,    'deploys/my_app'
+    set :app_path,    '#{@deploy_path}'
     set :www_workers, 2
 
     #{macro}
@@ -25,8 +27,59 @@ Given /^I make the initial deployment$/ do
   deploy_recipe_run
 end
 
+Then /^the deployed app repository must be cloned$/ do
+  in_current_dir do
+    expect(`git -C #{@deploy_path} log --oneline -1`)
+      .to include 'Add generated rails app'
+  end
+end
+
 Then /^the deployed app repository must be up to date$/ do
   in_current_dir do
-    expect(`git -C deploys/my_app log --oneline -1`).to include 'Make change'
+    expect(`git -C #{@deploy_path} log --oneline -1`).to include 'Make change'
   end
+end
+
+Then /^the deployed app must have its database connection configured$/ do
+  check_file_content "#{@deploy_path}/config/database.yml", <<-eoh
+default: &default
+  adapter: postgresql
+  encoding: unicode
+  pool: 5
+
+production:
+  <<: *default
+  database: some_host_test
+  eoh
+end
+
+Then /^the deployed app must have its dependencies installed$/ do
+  check_file_presence "#{@deploy_path}/Gemfile.lock", true
+end
+
+Then /^the deployed app must have its database migrations up$/ do
+  unset_bundler_env_vars
+  with_env 'RUBYLIB' => nil, 'RAILS_ENV' => 'production' do
+    in_current_dir do
+      expect(`cd #{@deploy_path} && bundle exec rake db:migrate:status`)
+        .to match /up\s+\d+\s+Create users/
+    end
+  end
+end
+
+Then /^the deployed app must have secret key setup$/ do
+  in_current_dir do
+    secrets = YAML.load(File.read("#{@deploy_path}/config/secrets.yml"))
+    expect(secrets['production']['secret_key_base']).to be
+  end
+end
+
+Then /^the deployed app must have unicorn configuration$/ do
+  check_file_content "#{@deploy_path}/config/unicorn.rb", <<-eoh
+worker_processes  2
+timeout           60
+preload_app       false
+pid               'tmp/run/www.pid'
+listen            "\#{ENV['HOME']}/#{@deploy_path}/tmp/run/www.sock"
+  eoh
 end
