549e8d3701b5866a7943675362fd872cc0e74f7f
Thibault Jouan <tj@a13.fr>
2015-05-06 04:25:26 +0000

    Workaround travis CI and RVM ruby hacks in UAT

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,11 +5,31 @@ GEMRC = "gem: --no-ri --no-rdoc\n".freeze
 
 Before('@sshd_gem_env') do
   write_file '.gemrc', GEMRC
-  write_file '.ssh/rc', <<-eoh
+
+  if ENV.key? 'TRAVIS'
+    ssh_rc = <<-eoh
+export GEM_HOME=#{ENV['GEM_HOME']}
+export GEM_PATH=#{ENV['GEM_PATH']}
+export MY_RUBY_HOME=#{ENV['MY_RUBY_HOME']}
+export PATH=#{ENV['PATH']}
+export RUBYLIB=#{ENV['RUBYLIB']}
+export rvm_autoupdate_flag=#{ENV['rvm_autoupdate_flag']}
+export rvm_bin_path=#{ENV['rvm_bin_path']}
+export rvm_path=#{ENV['rvm_path']}
+export rvm_prefix=#{ENV['rvm_prefix']}
+export rvm_silence_path_mismatch_check_flag=#{ENV['rvm_silence_path_mismatch_check_flag']}
+export rvm_version="#{ENV['rvm_version']}"
+export RAILS_ENV=production
+rvm use #{ENV['TRAVIS_RUBY_VERSION']}
+    eoh
+  else
+    ssh_rc = <<-eoh
 export GEM_HOME=#{ENV['GEM_HOME']}
 export PATH=#{ENV['GEM_HOME']}/bin:$PATH
 export RAILS_ENV=production
-  eoh
+    eoh
+  end
+  write_file '.ssh/rc', ssh_rc
 end
 
 After('@unicorn_kill') do
